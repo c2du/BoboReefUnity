38175e137f355f8eff4ecb07ed5aca45bb68c0fb
Connie Du <c2du@ucsd.edu>
2016-09-02 13:08:16 -0700

    Start Menu Added

---
+++ Assets/Scripts/Opacity.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Opacity : MonoBehaviour {
+
+	Image image;
+
+	// Use this for initialization
+	void Start () {
+		image = GetComponent<Image> ();
+
+		Color c = image.color;
+		c.a = 0.7f;
+		image.color = c;
+	}
+}
