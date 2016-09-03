4e4baac4b5c0bb51c80378449e8abe08743bba5c
Connie Du <c2du@ucsd.edu>
2016-09-02 18:50:08 -0700

    Fixed start button

---
+++ Assets/Scripts/LoadOnClickMain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadOnClickMain : MonoBehaviour {
+
+	public GameObject loadingImage;
+
+	public void LoadScene(int scene)
+	{
+		loadingImage.SetActive (true);
+		SceneManager.LoadScene (scene);
+		Debug.Log ("Clicking start button");
+	}
+}
